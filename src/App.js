b8917f26033132fcc26f49f093b710420430ca97
Hafid <hafid.f.septiawan@gmail.com>
2022-07-21 15:14:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import { useMemo } from "react";
 import { useRef } from "react";
 import { useEffect, useState } from "react";
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-// import { HashLink as Link } from "react-router-hash-link";
 
 const pr = [
   {
     _id: "2",
     name: "Kanban Board",
-    desc: "Kanban board app (Trello clone) built with MERN STACK",
-    tags: ["NodeJS", "ExpressJS", "ReactJS", "MongoDB", "SocketIO"],
+    desc: "Trello clone built with MERN STACK",
+    tags: ["js", "node", "express", "react", "mongo", "sio"],
     image: "/img/mern-kanboard/mk2c.png",
     images: [
       "/img/mern-kanboard/mk1.png",
@@ -25,7 +24,7 @@ const pr = [
     _id: "3",
     name: "Media Server",
     desc: "Simple media server app",
-    tags: ["Javascript", "ExpressJS", "Google Drive API"],
+    tags: ["js", "express", "react", "tailwind"],
     image: "/img/media-server/ms4c.png",
     images: [
       "/img/media-server/ms1.png",
@@ -47,7 +46,7 @@ const pr = [
     _id: "1",
     name: "Simple Chat",
     desc: "Realtime chatting app built with MERN STACK",
-    tags: ["NodeJS", "ExpressJS", "ReactJS", "MongoDB", "SocketIO"],
+    tags: ["js", "node", "express", "react", "mongo", "sio", "bootstrap"],
     image: "/img/mern-chat/mc2c.png",
     images: [
       "/img/mern-chat/mc1.png",
@@ -66,7 +65,7 @@ const pr = [
     _id: "4",
     name: "E-Learning",
     desc: "E-Learning app built with Laravel and ReactJS",
-    tags: ["PHP", "Laravel", "ReactJS", "PostgreSQL"],
+    tags: ["php", "laravel", "react", "sql"],
     image: "/img/reavelearning/el9c.png",
     images: [
       "/img/reavelearning/el1.png",
@@ -86,51 +85,132 @@ const pr = [
     },
   },
 ];
+
 const tcs = [
-  "Javascript",
-  "NodeJS",
-  "ExpressJS",
-
-  "ReactJS",
-
-  "PHP",
-  "SQL",
-  "Laravel",
-  "Bootstrap",
-  "Tailwind CSS",
+  {
+    id: "js",
+    text: (
+      <>
+        <i className="fa-brands fa-js"></i> Javascript
+      </>
+    ),
+  },
+  {
+    id: "node",
+    text: (
+      <>
+        <i className="fa-brands fa-node"></i> NodeJS
+      </>
+    ),
+  },
+  {
+    id: "express",
+    text: (
+      <>
+        <i className="fa-brands fa-node"></i> ExpressJS
+      </>
+    ),
+  },
+  {
+    id: "react",
+    text: (
+      <>
+        <i className="fa-brands fa-react"></i> ReactJS
+      </>
+    ),
+  },
+  {
+    id: "sio",
+    text: (
+      <>
+        <i className="fa-solid fa-bolt"></i> Socket.IO
+      </>
+    ),
+  },
+  {
+    id: "php",
+    text: (
+      <>
+        <i className="fa-brands fa-php"></i> PHP
+      </>
+    ),
+  },
+  {
+    id: "sql",
+    text: (
+      <>
+        <i className="fa-solid fa-database"></i> SQL
+      </>
+    ),
+  },
+  {
+    id: "mongo",
+    text: (
+      <>
+        <i className="fa-solid fa-database"></i> MongoDB
+      </>
+    ),
+  },
+  {
+    id: "laravel",
+    text: (
+      <>
+        <i className="fa-brands fa-laravel"></i> Laravel
+      </>
+    ),
+  },
+  {
+    id: "bootstrap",
+    text: (
+      <>
+        <i className="fa-brands fa-css3-alt"></i> Bootstrap
+      </>
+    ),
+  },
+  {
+    id: "tailwind",
+    text: (
+      <>
+        <i className="fa-brands fa-css3-alt"></i> Tailwind
+      </>
+    ),
+  },
 ];
 
-let darkMode = false;
+let darkMode = true;
 try {
   darkMode = JSON.parse(localStorage.getItem("darkMode"));
 } catch (error) {}
 
-const name = "Abcde Fghijklmn Opqrst";
-const about =
-  "Hello! My name is Paijo. Born and raised in Yogyakarta. I am always curious and willing to learn new things.";
+const name = "Hafid Fadilah Septiawan";
+// const about =
+//   "Hello! My name is Paijo. Born and raised in Yogyakarta. I am always curious and willing to learn new things.";
 const github = "https://github.com/hafidd";
 
 function App() {
-  const [projects, setProjects] = useState(pr);
+  const [projects, setProjects] = useState([]);
 
   const [project, setProject] = useState({});
 
   const [scrolling, setScrolling] = useState(false);
-  const [apalah, setApalah] = useState(false);
+  const [apalah, setApalah] = useState(false); // is scrolled
 
   const [dark, setDark] = useState(darkMode);
 
   const aboutRef = useRef(null);
   const projectRef = useRef(null);
 
+  useEffect(() => setProjects(pr), [pr]);
+
+  // scroll event
   let to = useRef(null);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 50) setApalah(true);
       else setApalah(false);
       setScrolling(true);
-      clearTimeout(to);
-      to = setTimeout(() => setScrolling(false), 222);
+      clearTimeout(to.current);
+      to.current = setTimeout(() => setScrolling(false), 222);
     });
   }, []);
 
@@ -143,8 +223,13 @@ function App() {
   return (
     <div className={`${dark ? "dark" : ""}`}>
       {/* Modal detail projek */}
-      {project._id && <Project project={project} setProject={setProject} />}
-      {/* Pencetan */}
+      {project._id && (
+        <>
+          <div className="fixed w-full h-screen bg-black z-10 bg-opacity-70"></div>
+          <Project project={project} setProject={setProject} />
+        </>
+      )}
+      {/* Menu */}
       <Menu
         apalah={apalah}
         scrolling={scrolling}
@@ -157,7 +242,11 @@ function App() {
       <div
         className={`bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-200 z-0`}
       >
-        <About aboutRef={aboutRef} />
+        <About
+          aboutRef={aboutRef}
+          apalah={apalah}
+          scrollToProject={scrollToProject}
+        />
         <Projects
           projectRef={projectRef}
           projects={projects}
@@ -179,49 +268,44 @@ function Menu({
 }) {
   return (
     <div
-      className={`fixed flex md:flex-col items-center justify-center h-16 md:h-screen w-full md:w-14 dark:text-slate-100 ${
-        apalah && "bg-black bg-opacity-80 md:bg-transparent"
+      className={`fixed flex md:flex-col items-center justify-center h-16 md:h-screen w-full md:w-14 lg:ml-4 dark:text-slate-100 ${
+        apalah &&
+        "bg-black bg-opacity-80 md:bg-transparent text-white md:text-black"
       }`}
     >
-      <a
-        href="#"
+      <button
         className={`text-center md:block p-2 px-3 md:mb-4 ${
-          scrolling && "md:border rounded-md"
+          scrolling && "bg-black bg-opacity-30 rounded-md"
         }`}
-        onClick={(e) => {
-          e.preventDefault();
-          scrollToAbout();
-        }}
+        onClick={(e) => scrollToAbout()}
       >
-        A
-      </a>
-      <a
-        href="#"
+        <i className="fa-solid fa-user"></i>
+      </button>
+      <button
         className={`text-center md:block p-2 px-3 md:mb-4 ${
-          scrolling && "md:border rounded-md"
+          scrolling && "bg-black bg-opacity-30 rounded-md"
         }`}
-        onClick={(e) => {
-          e.preventDefault();
-          scrollToProject();
-        }}
+        onClick={(e) => scrollToProject()}
       >
-        B
-      </a>
-      <a
-        href="#"
+        <i className="fa-solid fa-list"></i>
+      </button>
+      <button
         className={`text-center md:block p-2 px-3 md:mb-4 ${
-          scrolling && "md:border rounded-md"
+          scrolling && "bg-black bg-opacity-30 rounded-md"
         }`}
-        onClick={(e) => {
-          e.preventDefault();
+        onClick={(e) =>
           setDark((prev) => {
             localStorage.setItem("darkMode", JSON.stringify(!prev));
             return !prev;
-          });
-        }}
+          })
+        }
       >
-        {dark ? "N" : "D"}
-      </a>
+        {dark ? (
+          <i className="fa-solid fa-moon"></i>
+        ) : (
+          <i className="fa-solid fa-sun"></i>
+        )}
+      </button>
     </div>
   );
 }
@@ -235,7 +319,7 @@ function Project({ project = {}, setProject }) {
         onClick={() => setProject({})}
       >
         <div
-          className="w-11/12 flex flex-col items-center justify-center z-40 bg-opacity-100 p-4 bg-white dark:bg-slate-700 dark:text-white"
+          className="w-11/12 flex flex-col items-center justify-center z-40 bg-opacity-100 p-4 bg-white dark:bg-slate-900 dark:text-white"
           onClick={(e) => e.stopPropagation()}
         >
           <h2 className="text-2xl mb-5 lg:text-4xl">{project.name}</h2>
@@ -243,7 +327,7 @@ function Project({ project = {}, setProject }) {
             <img
               src={project.images[imgIndex]}
               alt={project.images[imgIndex]}
-              className="min-h-[30vh] max-h-[60vh] max-w-[80vw]"
+              className="min-h-[50vh] max-h-[60vh] max-w-[80vw]"
               loading="lazy"
             />
             {project.images.length > 1 && (
@@ -300,7 +384,7 @@ function Project({ project = {}, setProject }) {
   );
 }
 
-function About({ aboutRef }) {
+function About({ aboutRef, apalah, scrollToProject }) {
   return (
     <div
       ref={aboutRef}
@@ -308,77 +392,168 @@ function About({ aboutRef }) {
     >
       <div className="flex flex-col items-center justify-center">
         <p className="lg:self-start te text-lg mb-1">Hi!, my name is</p>
-        <h2 className="font-semibold mb-6 text-center text-5xl md:text-6xl lg:text-7xl lg:self-start">
+        <h2 className="font-semibold mb-6 text-center text-5xl md:text-6xl lg:text-7xl lg:self-start spaci">
           {name || "My name"}
         </h2>
-        <p className="font-semibold lg:self-start">About me .</p>
+        {/* <p className="font-semibold lg:self-start">About me .</p>
         <p className="mb-3 indent-7 p-5 lg:self-start lg:p-0">
           {about || "about me..."}
-        </p>
-        <a
-          className="py-1 px-2 border lg:self-start"
-          href={github}
-          target="_blank"
-          rel="noreferrer"
-        >
-          My Github repository
-        </a>
+        </p> */}
+        <div className={!apalah ? "lg:self-start" : ""}>
+          <a href={github} target="_blank" rel="noreferrer">
+            <button className={"mr-2 py-2 px-3 border-2 rounded-md "}>
+              <i className="fa-brands fa-github"></i> Github repository
+            </button>
+          </a>
+          <button
+            className={"py-2 px-3 border-2 rounded-md " + (apalah && " hidden")}
+            onClick={() => scrollToProject()}
+          >
+            <i className="fa-solid fa-list"></i> Projects
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
 function Projects({ projects, tcs, setProject, projectRef }) {
-  return (
-    <div ref={projectRef} className="md:px-24 lg:px-48 py-4 dark:bg-slate-900">
-      {/* <h2 className="text-center mb-2 text-3xl">Projects</h2> */}
-      <div className="flex flex-wrap justify-center">
-        {tcs?.map((tc) => (
-          <div key={tc} className="p-1 m-1 border tracking-widest text-sm">
-            {tc}
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const Tags = () =>
+    tcs?.map((tc, i) => (
+      <button
+        key={tc.id || i}
+        className="relative rounded-sm p-1 m-1 border tracking-widest text-sm"
+        onClick={() => addFilter(tc.id)}
+      >
+        {selectedTags.includes(tc.id) && (
+          <div className="absolute bg-black w-full h-full left-0 top-0 rounded-sm bg-opacity-50"></div>
+        )}
+        {tc.text || "tag"}
+      </button>
+    ));
+
+  const SelectedTags = () =>
+    selectedTags?.map((tc, i) => (
+      <button
+        key={tc || i}
+        className="rounded-sm p-1 m-1 border tracking-widest text-sm"
+        onClick={() => removeFilter(tc)}
+      >
+        <>{tcs.find((t) => t.id === tc).text || "tag"} x</>
+      </button>
+    ));
+
+  const ProjectCard = ({ project }) => (
+    <li className="m-auto flex p-1 w-full md:w-1/2">
+      <div className="rounded-md m-auto w-full bg-white border-2 dark:border-none dark:bg-slate-700">
+        <div
+          className="p-1 hover:cursor-pointer"
+          onClick={() => setProject(project)}
+        >
+          <img
+            src={project.image}
+            alt={project.image}
+            className="rounded-md "
+          />
+        </div>
+        <div className="flex flex-col p-4">
+          <div className="uppercase text-xl font-semibold tracking-wide">
+            {project.name}
           </div>
-        ))}
+          <p className="flex-1 text-sm mb-2">{project.desc || "desc"}</p>
+          <div className="flex w-full">
+            {project.link.demo && (
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={project.link.demo}
+                className="rounded-sm block text-center border p-1 px-2 mx-1 w-1/2"
+              >
+                Demo
+              </a>
+            )}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={project.link.github}
+              className="rounded-sm block text-center border p-1 px-2 mx-1 w-1/2"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+
+  const filteredProjects = useMemo(
+    () =>
+      selectedTags.length === 0
+        ? projects
+        : projects.filter((project) =>
+            selectedTags.every((tag) => project.tags.includes(tag))
+          ),
+    [projects, selectedTags]
+  );
+
+  const addFilter = (tag) =>
+    setSelectedTags((prev) =>
+      prev.filter((t) => t === tag).length > 0 ? prev : [...prev, tag]
+    );
+  const removeFilter = (tag) =>
+    setSelectedTags((prev) => prev.filter((t) => t !== tag));
+
+  return (
+    <div
+      ref={projectRef}
+      className="md:px-24 lg:px-48 py-4 dark:bg-slate-900 min-h-screen"
+    >
+      <div className="flex flex-wrap justify-center">
+        <Tags />
       </div>
 
+      {selectedTags.length > 0 && (
+        <div className="flex flex-wrap justify-center mt-1 border-y py-1">
+          <span className="rounded-sm p-1 m-1 tracking-widest text-sm">
+            Filter({selectedTags.length}) :{" "}
+          </span>
+          <SelectedTags />
+          <button
+            className="rounded-sm p-1 m-1 border tracking-widest text-sm"
+            onClick={() => setSelectedTags([])}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+
       <ul className="flex flex-wrap p-1 lg:p-4">
-        {projects?.map((project) => (
-          <li className="m-auto flex p-1 w-full md:w-1/2" key={project._id}>
-            <div className="m-auto w-full bg-slate-50 dark:bg-slate-500">
-              <div
-                className="p-1 hover:cursor-pointer"
-                onClick={() => setProject(project)}
-              >
-                <img src={project.image} alt={project.image} />
-              </div>
-              <div className="flex flex-col p-4">
-                <div className="uppercase text-xl font-semibold tracking-wide">
-                  {project.name}
-                </div>
-                <p className="flex-1 text-sm mb-2">{project.desc || "desc"}</p>
-                <div className="flex w-full">
-                  {project.link.demo && (
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href={project.link.demo}
-                      className="block text-center border p-1 px-2 mx-1 w-1/2"
-                    >
-                      Demo
-                    </a>
-                  )}
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href={project.link.github}
-                    className="block text-center border p-1 px-2 mx-1 w-1/2"
-                  >
-                    Github
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
+        {filteredProjects.length > 0 ? (
+          filteredProjects.map((project) => (
+            <ProjectCard project={project} key={project._id} />
+          ))
+        ) : (
+          <div className="w-full text-center">
+            No data found...{" "}
+            <a
+              href={github + "?tab=repositories"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className={"mx-1 py-1 px-2 border rounded-md"}>
+                <i className="fa-brands fa-github"></i> Github repository
+              </button>
+            </a>
+            <button
+              className="mx-1 py-1 px-2 border rounded-md"
+              onClick={() => setSelectedTags([])}
+            >
+              Clear filter
+            </button>
+          </div>
+        )}
       </ul>
     </div>
   );
